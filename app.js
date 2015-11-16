e7624febb2b6aa3859c37b14f40f32399f3674a7
Sidney Andrews <sidney@seesharprun.net>
2015-11-16 14:37:15 -0500

    Updated template with Bootstrap and angular

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var indexRoute = require('./routes/index');
+var demoRoute = require('./routes/demo');
 
 var app = express();
 
@@ -22,8 +21,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', indexRoute);
+app.use('/demo', demoRoute);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
