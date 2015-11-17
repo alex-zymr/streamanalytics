5749fe082e12d4509d4521bbf492be63b370d110
Sidney Andrews <sidney@seesharprun.net>
2015-11-17 00:56:18 -0500

    Shortened Title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET demo page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Event Hubs & Stream Analytics Demo' });
+  res.render('index', { title: 'Event Hubs Demo' });
 });
 
 module.exports = router;
